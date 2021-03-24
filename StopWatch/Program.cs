3b8d70e040b284082b9df997e78f01b028c16b67
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-03-23 22:58:59 -0300

    up

---
+++ StopWatch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+    static void Menu()
+    {
+        System.Console.WriteLine("S = Seg");
+        System.Console.WriteLine("0 = Sair");
+        System.Console.WriteLine("M = Min");
+        System.Console.WriteLine("Digite um número:");
+        
+        //pegando dados
+        string data = Console.ReadLine().ToLower();
+
+        //taking the last 
+        char type = char.Parse(data.Substring(data.Length - 1,1)); 
+        
+        //taking the firsts 
+        int time = int.Parse(data.Substring(0, data.Length -1)); 
+         //Start(value);
+
+         int multiplier = 1;
+
+         if(type == 'm') multiplier = 60; 
+         if(time == 0) System.Environment.Exit(0);
+
+         Start(time * multiplier);
+
+
+
+    }
+        static void Start(int time)
+        {
+            
+            int currentTime = 0;
+
+            while(time != currentTime)
+            {
+                Console.Clear();
+                currentTime++;
+                System.Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            System.Console.WriteLine("finish");
+            Thread.Sleep(1500);
+        }
+    }
+}
---
+++ fundamentals_code/parteUm/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+namespace parteUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu(){
+              Console.WriteLine("Digite um numero: ");
+            double numero_um = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro numero: ");
+            Console.WriteLine("Soma: +");
+            Console.WriteLine("Divisão: /");
+            Console.WriteLine("Subtração: -");
+            Console.WriteLine("Resto: %");
+            Console.WriteLine("Multiplicação: *");
+            Console.WriteLine("Potencia: **");
+            Console.WriteLine("Raiz: r");
+            Console.WriteLine("Menu: M");
+            Console.WriteLine("SAIR: e" );
+            string op = Console.ReadLine();
+            double numero_dois = double.Parse(Console.ReadLine());
+            Console.WriteLine("Selecione a operação: ");
+
+            switch (op)
+            {               
+                case "+": Soma(numero_um, numero_dois); break;
+                case "-": Subtracao(numero_um, numero_dois); break;
+                case "/": Divisao(numero_um, numero_dois); break;
+                case "*": Multiplicacao(numero_um, numero_dois); break;
+                case "**": Potencia(numero_um, numero_dois); break;
+                case "r": raiz(numero_um, numero_dois); break;
+                case "m": Menu(); break;
+                case "e": System.Environment.Exit(0); break; //encerrar a aplicação
+                default: Console.WriteLine("Sem resposta"); break;
+            }
+        }
+
+        static void raiz(double valor, double valor2)
+        {
+            var value = Math.Sqrt(valor);
+            var value2 = Math.Sqrt(valor2);
+            Console.WriteLine(value.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(value2.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static void Soma(double valor,  double valor2)
+        {
+            var value = (valor + valor2);
+            Console.WriteLine(value.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+         static void Subtracao(double valor,  double valor2)
+        {
+            var value = (valor - valor2);
+            Console.WriteLine(value.ToString("F2", CultureInfo.InvariantCulture));
+        }
+         static void Divisao(double valor,  double valor2)
+        {
+            var value =  (valor / valor2);
+            Console.WriteLine(value.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+         static void Multiplicacao(double valor,  double valor2)
+        {
+            var value =  (valor * valor2);
+            Console.WriteLine(value.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static void Potencia(double valor,  double valor2)
+        {
+            var value =  Math.Pow(valor, valor2);
+            Console.WriteLine(value.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
