29c53bbaa21b1ae4a7565adf4ccd5e0ec40da6b0
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-03-25 17:26:53 -0300

    htmleditor

---
+++ HTMLEditor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HTMLEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+            Menu.DrawScreen();
+        }
+    }
+}
