092e3dc14c187eb2688428f619bbab90f017cbbd
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-03-26 15:45:13 -0300

    atz

--- WorkDatas2/Program.cs
+++ WorkDatas2/Program.cs
@@ -6,7 +6,19 @@ namespace WorkDatas2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            var data = DateTime.Now;
+            var formatada = String.Format("{0:y}",data);
+            var formatada1 = String.Format("{0:yy}",data);
+            var formatada2 = String.Format("{0:yyyy}",data);
+            var formatada3 = String.Format("{0:M}",data);
+            var formatada4 = String.Format("{0:dd/MM/yyyy hh:mm:ss z}",data);
+            // var formatada3 = String.Format("{0:y}",data);
+            Console.WriteLine(formatada);
+            Console.WriteLine(formatada1);
+            Console.WriteLine(formatada2);
+            Console.WriteLine(formatada3);
+            Console.WriteLine(formatada4);
         }
     }
 }
---
+++ WorkDatasAddDays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkDatasAddDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
