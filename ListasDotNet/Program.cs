6a9d27b5cb1a67dc56fa8cd97291895db6a1f775
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-03-26 23:17:49 -0300

    finalizando curso

---
+++ ListasDotNet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ListasDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var meuArray = new int[5] {0, 1, 2, 3, 4};
+       
+        for (var i = 0; i < meuArray.Length; i++) System.Console.WriteLine(meuArray[i]);
+        
+        foreach (var item in meuArray)  System.Console.WriteLine(item);
+        
+
+        }
+    }
+}
