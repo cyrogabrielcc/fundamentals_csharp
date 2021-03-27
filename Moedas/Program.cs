37e11522f0ad2c25a2005de2bcd2d66b062c991b
cyrogabrielcc <cyrocunhacg@gmail.com>
2021-03-26 22:33:08 -0300

    add cap moedas

---
+++ Moedas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal value = 10.50m;
+            Console.WriteLine(value.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(value.ToString("P", CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(value.ToString("N", CultureInfo.CreateSpecificCulture("pt-BR")));
+
+            
+            //arredondar pra cima ou pra baixo com a class Math - Roud, Ceiling e Floor
+        }
+    }
+}
